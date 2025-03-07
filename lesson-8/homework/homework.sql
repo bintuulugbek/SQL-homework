f2df1dbd361827f6f2abde6234a35f58517bc1f9
bintuulugbek <163106257+bintuulugbek@users.noreply.github.com>
2025-03-07 15:02:55 +0500

    Update homework.sql

--- lesson-8/homework/homework.sql
+++ lesson-8/homework/homework.sql
@@ -48,23 +48,24 @@ on e.departmentID = d.departmentID
 --10. Write a query that uses the ON clause with a <> operator to join two tables and return rows where the values in the two columns are not equal.
 select *
 from employees e inner join departments d
-on e.departmentID <> d.departmentID                           --CHECK IMPORTANT
+on e.departmentID <> d.deptID                   
 
 --## 🟠 Medium-Level Tasks (10)
 
---11. Write a query to demonstrate a One to Many relationship by joining the Customers and Orders tables using INNER JOIN, showing the CustomerName and the total number of orders.
-select customer_name, sum(quantity)
-from customers c inner join orders o                      ----------check
+--11. Write a query to demonstrate a One to Many relationship by joining the Customers and Orders tables using INNER JOIN, showing the CustomerName and the total number of orders.z2
+select o.customerID, count(orderID)
+from customers c inner join orders o
 on c.customerID = o.customerID
+group by o.customerID
 --12. Write a query to demonstrate a Many to Many relationship between Students and Courses by joining through the StudentCourses table.
-select
-from students s inner join courses c                 ------------------- check
-on 
 --13. Write a query to use a CROSS JOIN between Employees and Departments tables, and filter the results by Salary > 5000 using the WHERE clause.
 select *
 from employees e cross join department d
 where e.salary > 5000
 --14. Write a query to demonstrate a One to One relationship by joining the Employee and EmployeeDetails tables, showing each employee’s name and their details.
+select *
+from employee e inner join employeeDetails ed
+on e.empId = ed.empID
 --15. Write a query to perform an INNER JOIN between Products and Suppliers and use the WHERE clause to filter only products supplied by 'Supplier A'.
 select * 
 from products p inner join suppliers s
@@ -99,11 +100,40 @@ where s.quantity > 1000
 
 --21. Write a query that demonstrates a Many to Many relationship between Authors and Books using the intermediate AuthorBooks table, showing the AuthorName and the BookTitle.
 --22. Write a query to join Products and Categories using INNER JOIN, and filter the result to only include products where the CategoryName is not ‘Electronics’.
+select * 
+from products p inner join categories c
+on p.categoryID = c.categoryID
+where c.CategoryName <> 'Electronics'
 --23. Write a query to perform a CROSS JOIN between Orders and Products, and filter the result with a WHERE clause to show only orders where the quantity is greater than 100.
+select o.orderName, o.order_quantity
+from orders o cross join products p
+where o.order_quantity > 100
 --24. Write a query that joins the Employees and Departments tables and uses a logical operator in the ON clause to only return employees who have been with the company for over 5 years.
+select employee_name
+from employees e inner join departments d
+on e.departmentID = d.departmentID
+where e.working_year >= 5
 --25. Write a query to show the difference between INNER JOIN and LEFT JOIN by returning employees and their departments, ensuring that employees without departments are included in the left join.
+select name, d.deptName
+from employees e left join departments d
+on e.departmentID = d.deptID
+
+select name, d.deptName
+from employees e inner join departments d
+on e.departmentID = d.deptID
 --26. Write a query that uses a CROSS JOIN between Products and Suppliers, and filters the result using WHERE to include only suppliers that supply products in 'Category A'.
+select *
+from products p cross join suppliers s
+where s.category = 'Category A'
 --27. Write a query to demonstrate a One to Many relationship using INNER JOIN between Orders and Customers, and apply the >= operator to filter only customers with at least 10 orders.
 --28. Write a query to demonstrate the Many to Many relationship between Courses and Students, showing all courses and the number of students enrolled using the COUNT function.
 --29. Write a query to use a LEFT JOIN between Employees and Departments tables, and filter the result using the WHERE clause to show only employees in the 'Marketing' department.
+select * 
+from employees e left join departments d
+on d.deptID = e.DepartmentID
+where deptName = 'Marketing'
 --30. Write a query that uses the ON clause with <= operator to join two tables and return rows where the values in the columns meet the condition.
+select *
+from customerss c inner join orderss o
+on c.customerID = o.customerID
+where orderID <= 5
