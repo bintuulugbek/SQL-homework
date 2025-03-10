aaa7977c7c154e308b97190be5c83516f35113db
bintuulugbek <163106257+bintuulugbek@users.noreply.github.com>
2025-03-10 12:52:44 +0500

    Update homework.sql

--- lesson-9/homework/homework.sql
+++ lesson-9/homework/homework.sql
@@ -7,32 +7,29 @@
 --- **Outer Joins (LEFT, RIGHT, FULL)**
 --- **Self Join**
 
---Bahor Eshmirzayeva
-
 --## 🟢 Easy-Level Tasks (10)
-
 --1. Write a query to join Employees and Departments using an INNER JOIN, and apply a WHERE clause to show only employees whose salary is greater than 5000.
 select *
-from employees e inner join departments d
-on d.departmentID = e.departmentID
-where e.salary > 5000
+from Employees inner join Departments 
+on Departments.DepartmentID = Employees.DepartmentID
+where Employees.Salary > 5000
 --2. Write a query to join Customers and Orders using an INNER JOIN, and apply the WHERE clause to return only orders placed in 2023.
 select * 
-from customers c inner join orders o
-on c.customerID = o.customerID
-where year = 2023
+from Customers inner join Orders
+on Customers.CustomerID = Orders.CustomerID
+where year(Orders.OrderDate) = 2023
 --3. Write a query to demonstrate a LEFT OUTER JOIN between Employees and Departments, showing all employees and their respective departments (including employees without departments).
-select e.employeeName, d.departmentName
-from employees e left outer join departments d
-on e.departmentID = d.departmentID 
+select Employees.EmployeeName, Departments.DepartmentName
+from Employees left outer join Departments
+on Employees.DepartmentID = Departments.DepartmentID 
 --4. Write a query to perform a RIGHT OUTER JOIN between Products and Suppliers, showing all suppliers and the products they supply (including suppliers without products).
-select s.supplierName, p.productName
-from products p right outer join suppliers s
-on s.supplierID = p.supplierID
+select Suppliers.SupplierName, Products.ProductName
+from Products right outer join Suppliers
+on Suppliers.SupplierID = Products.SupplierID
 --5. Write a query to demonstrate a FULL OUTER JOIN between Orders and Payments, showing all orders and their corresponding payments (including orders without payments and payments without orders).
-select o.orderName, p.payment
-from orders o full outer join payments p
-on o.paymentID = p.paymentID
+select Orders.OrderName, P.Payment
+from Orders full outer join Payments
+on Orders.PaymentID = Payments.PaymentID
 --6. Write a query to perform a SELF JOIN on the Employees table to display employees and their respective managers (showing EmployeeName and ManagerName).
 select Manager.Name as Employee, Employee.Name as Manager
 from Employees as Manager join Employees as Employee
@@ -64,7 +61,6 @@ on Employees.DepartmentID = Employees.DepartmentID
 where Departments.Name = 'HR'
 
 --## 🟠 Medium-Level Tasks (10)
-
 --11. Write a query to perform an INNER JOIN between Employees and Departments, and use the WHERE clause to show employees who belong to departments with more than 10 employees.
 select Employees.Name
 from Employees
@@ -95,24 +91,122 @@ full outer join Departments
 on Employees.DepartmentID = Departments.DepartmentID
 where Departments.DepartmentID is null
 --15. Write a query to perform a SELF JOIN on the Employees table to show employees who report to the same manager.
---16. Write a query that uses the logical order of SQL execution to perform a LEFT OUTER JOIN between Orders and Customers, followed by a WHERE clause to filter orders placed in the year 2022.
+select Manager.Name as Employee, Employee.Name as Manager
+from Employees as Manager
+join Employees as Employee
+on Employee.ID = Manager.ManagerID  
+--16. Write a query that uses the logical order of SQL execution to perform a LEFT OUTER JOIN between Orders and Customers, 
+--followed by a WHERE clause to filter orders placed in the year 2022.
+select *
+from Orders
+left outer join Customers
+on Orders.CustomerID = Customer.CustomerID
+where year(Orders.OrderDate) = 2022
 --17. Write a query to use the ON clause with INNER JOIN to return only the employees from the 'Sales' department whose salary is greater than 5000.
+select *
+from Sales 
+inner join Departments
+on Sales.DepartmentID = Departments.DepartmentID
+where Sales.department = 'Sales' and Sales.salary > 5000
 --18. Write a query to join Employees and Departments using INNER JOIN, and use WHERE to filter employees whose department's DepartmentName is 'IT'.
+select *
+from Employees
+inner join Departments
+on Employees.DepartmentID = Departments.DepartmentID
+where Departments.DepartmentName = 'IT'
 --19. Write a query to join Orders and Payments using a FULL OUTER JOIN, and use the WHERE clause to show only the orders that have corresponding payments.
+select *
+from Orders
+full outer join Payments
+on Orders.orderID = Payments.OrderID
+where Payments.OrderID is not null
 --20. Write a query to perform a LEFT OUTER JOIN between Products and Orders, and use the WHERE clause to show products that have no orders.
-
------
+select *
+from Products
+left outer join Orders
+on Products.ProductID = Orders.ProductID
+where Orders.OrderID is null
 
 --## 🔴 Hard-Level Tasks (10)
+--21. Write a query that explains the logical order of SQL execution by using a JOIN between Employees and Departments, 
+--followed by a WHERE clause to show employees whose salary is higher than the average salary of their department.
 
---21. Write a query that explains the logical order of SQL execution by using a JOIN between Employees and Departments, followed by a WHERE clause to show employees whose salary is higher than the average salary of their department.
+select *
+from Employees
+join (
+	select Employees.Department, avg(Employees.Salary) avg_salary
+	from Employees
+	group by Employees.Department
+) Department_avg_salary
+on Employees.Department = Department_avg_salary.Department
+where Employees.Salary > Department_avg_salary.avg_salary) 
+--Logical order of sql execustion =>  1. from Employees; 2. creates Department_avg_salary; 3. joins two tables according to ON condition; 4. where conditon; 5 select works
 --22. Write a query to perform a LEFT OUTER JOIN between Orders and Payments, and use the WHERE clause to return all orders placed before 2020 that have not been paid yet.
+select * 
+from Orders
+left outer join Payments
+on Orders.OrderID = Payments.OrderID
+where year(Orders.OrderDate) < 2020 and Payments.OrderID is null 
 --23. Write a query to perform a FULL OUTER JOIN between Products and Categories, and use the WHERE clause to filter only products that have no category assigned.
+select * 
+from Products
+full outer join Categories 
+on Products.CategoryID = Categories.CategoryID
+where Categories.CategoryID is null
 --24. Write a query to perform a SELF JOIN on the Employees table to find employees who report to the same manager and earn more than 5000.
+select Employee.Name as Employee, Employee.Salary as emp_sal, Manager.Name as Manager, Manager.Salary as man_sal
+from Employees as Manager
+inner join Employees as Employee
+on Manager.ID = Employee.ID
+where Employee.Salary > 5000
 --25. Write a query to join Employees and Departments using a RIGHT OUTER JOIN, and use the WHERE clause to show employees from departments where the department name starts with ‘M’.
---26. Write a query to demonstrate the difference between the ON clause and the WHERE clause by joining Products and Sales, and using WHERE to filter only sales greater than 1000.
+select Employees.Name, Employees.Department
+from Employees
+right outer join Departments 
+on Employees.Department = Departments.Name
+where Departments.Name like 'M%'
+--26. Write a query to demonstrate the difference between the ON clause and the WHERE clause by joining Products and Sales, 
+--and using WHERE to filter only sales greater than 1000.
+select * 
+from Products
+inner join Sales
+on Products.ProductID = Sales.ProductID
+where Sales.sales > 1000
+
+select *
+from Products
+inner join Sales
+on Products.ProductID = Sales.ProductID and Sales.sales > 1000
 --27. Write a query to perform a LEFT OUTER JOIN between Students and Courses, and use the WHERE clause to show only students who have not enrolled in 'Math 101'.
---28. Write a query that explains the logical order of SQL execution by using a FULL OUTER JOIN between Orders and Payments, followed by a WHERE clause to filter out the orders with no payment.
+select * 
+from Students
+left outer join Courses
+on Courses.CourseID = Students.CourseID
+where Courses.name <> 'Math 101' or Courses.Name is null
+--28. Write a query that explains the logical order of SQL execution by using a FULL OUTER JOIN between Orders and Payments, 
+--followed by a WHERE clause to filter out the orders with no payment.
+select *
+from Orders
+full outer join Payments
+on Orders.orderID = Payments.OrderID
+where Payments.OrderID is null
 --29. Write a query to join Products and Categories using an INNER JOIN, and use the WHERE clause to filter products that belong to either 'Electronics' or 'Furniture'.
+select * 
+from Products
+inner join Categories
+on Products.CategoryID = Categories.CategoryID
+where Categories.Name in ('Electronics', 'Furniture')
 --30. Write a query to perform a CROSS JOIN between Customers and Orders, and use the WHERE clause to filter the result to show customers with more than 2 orders placed in 2023.
+select Customers.CustomerID, Customers.Name, count(Orders.OrderID) OrdersIn2023
+from Customers  
+cross join Orders
+where Customers.CustomerID = Orders.CustomerID and year(Orders.OrderDate) = 2023
+group by Customers.CustomerID
+having count(orders.OrderID) > 2
+
+
+
+
+
+
 
